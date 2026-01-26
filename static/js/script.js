b3d41a43d6667e09f05dedd86cee0124d4d6ecb5
L-LiuY <120864724+LLKYTA@users.noreply.github.com>
2026-01-26 16:53:41 +0800

    2

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 console.log('%cCopyright © 2024 zyyo.net',
-    'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
+	'background-color: #ff00ff; color: white; font-size: 24px; font-weight: bold; padding: 10px;'
 );
 console.log('%c   /\\_/\\', 'color: #20128b; font-size: 20px;');
 console.log('%c  ( o.o )', 'color: #20128b; font-size: 20px;');
@@ -7,82 +7,82 @@ console.log(' %c  > ^ <', 'color: #20128b; font-size: 20px;');
 console.log('  %c /  ~ \\', 'color: #20128b font-size: 20px;');
 console.log('  %c/______\\', 'color: #20128b; font-size: 20px;');
 
-document.addEventListener('contextmenu', function (event) {
-    event.preventDefault();
+document.addEventListener('contextmenu', function(event) {
+	event.preventDefault();
 });
 
 function handlePress(event) {
-    this.classList.add('pressed');
+	this.classList.add('pressed');
 }
 
 function handleRelease(event) {
-    this.classList.remove('pressed');
+	this.classList.remove('pressed');
 }
 
 function handleCancel(event) {
-    this.classList.remove('pressed');
+	this.classList.remove('pressed');
 }
 
 var buttons = document.querySelectorAll('.projectItem');
-buttons.forEach(function (button) {
-    button.addEventListener('mousedown', handlePress);
-    button.addEventListener('mouseup', handleRelease);
-    button.addEventListener('mouseleave', handleCancel);
-    button.addEventListener('touchstart', handlePress);
-    button.addEventListener('touchend', handleRelease);
-    button.addEventListener('touchcancel', handleCancel);
+buttons.forEach(function(button) {
+	button.addEventListener('mousedown', handlePress);
+	button.addEventListener('mouseup', handleRelease);
+	button.addEventListener('mouseleave', handleCancel);
+	button.addEventListener('touchstart', handlePress);
+	button.addEventListener('touchend', handleRelease);
+	button.addEventListener('touchcancel', handleCancel);
 });
 
 function toggleClass(selector, className) {
-    var elements = document.querySelectorAll(selector);
-    elements.forEach(function (element) {
-        element.classList.toggle(className);
-    });
+	var elements = document.querySelectorAll(selector);
+	elements.forEach(function(element) {
+		element.classList.toggle(className);
+	});
 }
 
 function pop(imageURL) {
-    var tcMainElement = document.querySelector(".tc-img");
-    if (imageURL) {
-        tcMainElement.src = imageURL;
-    }
-    toggleClass(".tc-main", "active");
-    toggleClass(".tc", "active");
+	var tcMainElement = document.querySelector(".tc-img");
+	if (imageURL) {
+		tcMainElement.src = imageURL;
+	}
+	toggleClass(".tc-main", "active");
+	toggleClass(".tc", "active");
 }
 
 var tc = document.getElementsByClassName('tc');
 var tc_main = document.getElementsByClassName('tc-main');
-tc[0].addEventListener('click', function (event) {
-    pop();
+tc[0].addEventListener('click', function(event) {
+	pop();
 });
-tc_main[0].addEventListener('click', function (event) {
-    event.stopPropagation();
+tc_main[0].addEventListener('click', function(event) {
+	event.stopPropagation();
 });
 
 
 
 function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1, cookie.length);
-        }
-        if (cookie.indexOf(nameEQ) == 0) {
-            return cookie.substring(nameEQ.length, cookie.length);
-        }
-    }
-    return null;
+	var nameEQ = name + "=";
+	var cookies = document.cookie.split(';');
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i];
+		while (cookie.charAt(0) == ' ') {
+			cookie = cookie.substring(1, cookie.length);
+		}
+		if (cookie.indexOf(nameEQ) == 0) {
+			return cookie.substring(nameEQ.length, cookie.length);
+		}
+	}
+	return null;
 }
 
 
@@ -99,53 +99,28 @@ function getCookie(name) {
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
 
 
 
 
 
-    var html = document.querySelector('html');
-    var themeState = getCookie("themeState") || "Light";
-    var tanChiShe = document.getElementById("tanChiShe");
+	var html = document.querySelector('html');
+	var themeState = getCookie("themeState") || "Light";
+	var tanChiShe = document.getElementById("tanChiShe");
 
 
 
 
 
 
-    function changeTheme(theme) {
-        tanChiShe.src = "./static/svg/snake-" + theme + ".svg";
-        html.dataset.theme = theme;
-        setCookie("themeState", theme, 365);
-        themeState = theme;
-    }
-
-
-
-
-
-
-
-    var Checkbox = document.getElementById('myonoffswitch')
-    Checkbox.addEventListener('change', function () {
-        if (themeState == "Dark") {
-            changeTheme("Light");
-        } else if (themeState == "Light") {
-            changeTheme("Dark");
-        } else {
-            changeTheme("Dark");
-        }
-    });
-
-
-
-    if (themeState == "Dark") {
-        Checkbox.checked = false;
-    }
-
-    changeTheme(themeState);
+	function changeTheme(theme) {
+		tanChiShe.src = "./static/svg/snake-" + theme + ".svg";
+		html.dataset.theme = theme;
+		setCookie("themeState", theme, 365);
+		themeState = theme;
+	}
 
 
 
@@ -153,6 +128,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+	var Checkbox = document.getElementById('myonoffswitch')
+	Checkbox.addEventListener('change', function() {
+		if (themeState == "Dark") {
+			changeTheme("Light");
+		} else if (themeState == "Light") {
+			changeTheme("Dark");
+		} else {
+			changeTheme("Dark");
+		}
+	});
+
+
+
+	if (themeState == "Dark") {
+		Checkbox.checked = false;
+	}
+
+	changeTheme(themeState);
 
 
 
@@ -163,55 +156,62 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-   
 
-    var fpsElement = document.createElement('div');
-    fpsElement.id = 'fps';
-    fpsElement.style.zIndex = '10000';
-    fpsElement.style.position = 'fixed';
-    fpsElement.style.left = '0';
-    document.body.insertBefore(fpsElement, document.body.firstChild);
 
-    var showFPS = (function () {
-        var requestAnimationFrame = window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
 
-        var fps = 0,
-            last = Date.now(),
-            offset, step, appendFps;
 
-        step = function () {
-            offset = Date.now() - last;
-            fps += 1;
 
-            if (offset >= 1000) {
-                last += offset;
-                appendFps(fps);
-                fps = 0;
-            }
 
-            requestAnimationFrame(step);
-        };
 
-        appendFps = function (fpsValue) {
-            fpsElement.textContent = 'FPS: ' + fpsValue;
-        };
 
-        step();
-    })();
-    
-    
-    
-    //pop('./static/img/tz.jpg')
-    
-    
-    
+
+	var fpsElement = document.createElement('div');
+	fpsElement.id = 'fps';
+	fpsElement.style.zIndex = '10000';
+	fpsElement.style.position = 'fixed';
+	fpsElement.style.left = '0';
+	document.body.insertBefore(fpsElement, document.body.firstChild);
+
+	var showFPS = (function() {
+		var requestAnimationFrame = window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(callback) {
+				window.setTimeout(callback, 1000 / 60);
+			};
+
+		var fps = 0,
+			last = Date.now(),
+			offset, step, appendFps;
+
+		step = function() {
+			offset = Date.now() - last;
+			fps += 1;
+
+			if (offset >= 1000) {
+				last += offset;
+				appendFps(fps);
+				fps = 0;
+			}
+
+			requestAnimationFrame(step);
+		};
+
+		appendFps = function(fpsValue) {
+			fpsElement.textContent = 'FPS: ' + fpsValue;
+		};
+
+		step();
+	})();
+
+
+
+	//pop('./static/img/tz.jpg')
+
+
+
 });
 
 
@@ -219,28 +219,28 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var pageLoading = document.querySelector("#zyyo-loading");
 window.addEventListener('load', function() {
-    setTimeout(function () {
-        pageLoading.style.opacity = '0';
-    }, 100);
+	setTimeout(function() {
+		pageLoading.style.opacity = '0';
+	}, 100);
 });
 
 
 fetch('https://v1.hitokoto.cn/?c=j&c=i')
-    .then(response => response.json())
-    .then(data => {
-      const hitokoto = document.querySelector('#hitokoto_text')
-	  const hitokoto_from = document.querySelector('#hitokoto_from')
-      hitokoto.href = `https://hitokoto.cn/?uuid=${data.uuid}`
-	  if (data.from_who = "null") {
-		hitokoto.from = "---" + data.from 	
-	  } else{
-	  	hitokoto.from = "---" + data.from + " " + data.from_who
-	  }
-      hitokoto.innerText = data.hitokoto
-	  hitokoto_from.innerText = hitokoto.from
-	  console.log(data)
-	  })
-    .catch(console.error)
+	.then(response => response.json())
+	.then(data => {
+		const hitokoto = document.querySelector('#hitokoto_text')
+		const hitokoto_from = document.querySelector('#hitokoto_from')
+		hitokoto.href = `https://hitokoto.cn/?uuid=${data.uuid}`
+		if (data.from_who = "null") {
+			hitokoto.from = "---" + data.from
+		} else {
+			hitokoto.from = "---" + data.from + " " + data.from_who
+		}
+		hitokoto.innerText = data.hitokoto
+		hitokoto_from.innerText = hitokoto.from
+		console.log(data)
+	})
+	.catch(console.error)
 
 // 	  数据结构一言
 //{
@@ -259,26 +259,20 @@ fetch('https://v1.hitokoto.cn/?c=j&c=i')
 // }
 
 
-// const SeverName = document.querySelector('.SeverName') 
-// SeverName.innerText = "自然之域"
-// SeverIp = "play.simpfun.cn:15436"
+const SeverName = document.querySelector('.SeverName')
+SeverName.innerText = "自然之域(MCsever)"
+SeverIp = "play.simpfun.cn:15436"
 
-// fetch('https://uapis.cn/api/v1/game/minecraft/serverstatus?server=' + SeverIp)
-// 	.then(response => response.json())
-// 	.then(data =>{
-// 		const ResponesData = data
-// 		console.log(ResponesData)
-		
-		
-		
-// 		const online = document.querySelector('.SeverStatus') 
-// 		if (data.online = true) {
-// 			online.innerText = "运行中"
-// 		} else{
-// 			online.innerText = "已关闭"
-// 		}
-		
-// 		c
-		
-// 	})
-
+fetch('https://uapis.cn/api/v1/game/minecraft/serverstatus?server=' + SeverIp)
+	.then(response => response.json())
+	.then(data => {
+		const ResponesData = data
+		console.log(data)
+		const online = document.querySelector('.SeverStatus')
+		if (data.online = true) {
+			online.innerText = "online"
+		} else {
+			online.innerText = "off"
+			online.style.backgroundColor = 'rgba(191, 0, 0, 1.0)'
+		}
+	})
