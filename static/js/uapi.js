8d9785b12ac1395ad780e87f463e45d36620aa58
LLKYTA <lys010107@163.com>
2026-06-05 11:28:37 +0800

    Updated some stuff and optimized the JavaScript logic

--- static/js/uapi.js
+++ static/js/uapi.js
@@ -1,1 +1,14 @@
-
+let client;
+async function UapiPreLoad() {
+  try {
+    let { UapiClient } = await import('https://cdn.jsdelivr.net/npm/uapi-browser-sdk@latest/dist/index.js')
+    client = new UapiClient('https://uapis.cn');
+  }
+  catch (err) {
+    console.error("Something wrong :" + err);
+    return 0;
+  }
+  console.log("UapiPreLoad success")
+}
+UapiPreLoad()
+// Uapi资源预加载
